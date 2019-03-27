e5d35949c168403dfd8636ca4fea20ab1f59712a
Verdiane <kengnelydiane684@gmail.com>
2019-03-27 01:36:49 +0100

    refactor: remove all the obsolete parts of the code and update the code on the client list page
    Fixes: (#214)

--- src/app/clients/clients.service.ts
+++ src/app/clients/clients.service.ts
@@ -1,347 +1,175 @@
+/** Angular Imports */
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response, RequestOptions, URLSearchParams } from '@angular/http';
-/* import { HttpHeaders, HttpClient } from '@angular/common/http';
- */
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+/** rxjs Imports */
 import { Observable } from 'rxjs';
-import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-@Injectable()
+/**
+ * Client service.
+ */
+@Injectable({
+  providedIn: 'root'
+})
 export class ClientsService {
-  constructor(private http: Http) {} // tslint:disable-line
 
-  createAuthorizationHeader(headers: Headers) { // tslint:disable-line
-    const username = 'mifos';
-    const password = 'password';
-    headers.append('Authorization', 'Basic ' + btoa(username + ':' + password));
-    headers.append('Content-Type', 'application/json');
-    headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    headers.append('Access-Control-Allow-Origin', '*');
+  /**
+   * @param {HttpClient} http Http Client to send requests.
+   */
+  constructor(private http: HttpClient) { }
+
+  /**
+   * @returns {Observable<any>} Clients data
+   */
+  getClients(): Observable<any> {
+    return this.http.get('/clients');
   }
 
-  getClients() {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get(
-        'https://demo.openmf.org/fineract-provider/api/v1/clients?tenantIdentifier=default&pretty=true', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response['pageItems'];
-
-          }
-        )
-      );
-  }
-  getClientId(clientId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '?tenantIdentifier=default&pretty=true', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response;
-
-          }
-        )
-      );
+  /**
+   * @param clientId
+   * @returns {Observable<any>} Client
+   */
+  getClientId(clientId: number): Observable<any> {
+    return this.http.get(`/clients/${clientId}`);
   }
 
+  /**
+   * @param clientId
+   * @returns {Observable<any>} Clients Accounts
+   */
   getClientAccounts(clientId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/accounts' + '?tenantIdentifier=default&pretty=true', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response;
-
-          }
-        )
-      );
+    return this.http.get(`/clients/${clientId}` + '/accounts');
   }
 
+  /**
+   * @param clientId
+   * @returns {Observable<any>} Clients Charges
+   */
   getClientCharges(clientId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/charges' + '?tenantIdentifier=default&pretty=true', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response;
-
-          }
-        )
-      );
+    return this.http.get(`/clients/${clientId}` + '/charges');
   }
 
+  /**
+   * @param clientId
+   * @returns {Observable<any>} Clients Addresses
+   */
   getClientAddress(clientId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/client/' + clientId +
-        '/addresses' + '?tenantIdentifier=default&pretty=true', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response;
-
-          }
-        )
-      );
+    return this.http.get(`client/${clientId}` + '/addresses');
   }
 
+  /**
+   * @param clientId
+   * @returns {Observable<any>} Clients Note
+   */
   getClientNote(clientId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/notes' + '?tenantIdentifier=default&pretty=true', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response;
-
-          }
-        )
-      );
+    return this.http.get(`clients/${clientId}` + '/notes');
   }
 
-  postClientNote(clientId: number, notes: any) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    const body = JSON.stringify(notes);
-    return this.http.post('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/notes' + '?tenantIdentifier=default&pretty=true', notes, {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response.json();
-          }
-        )
-      );
+  /**
+   * @param clientId
+   * @param notes
+   * @returns {Observable<any>}
+   */
+  postClientNote(clientId: number, notes: any): Observable<any> {
+    return this.http.post(`/clients/${clientId}` + '/notes', notes);
   }
 
-  getClientAddressTemplate() {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/client/' +
-        'addresses/template?tenantIdentifier=default', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response;
-
-          }
-        )
-      );
+  /**
+   * @returns {Observable<any>} Client Address template.
+   */
+  getClientAddressTemplate(): Observable<any> {
+    return this.http.get('/clients/addresses/template');
   }
 
-  postClientAddress(clientId: number, body: any, type: any) {
-    const headers = new Headers(); // tslint:disable-line
-    const params = new URLSearchParams(); // tslint:disable-line
-    params.set('type', type);
-    this.createAuthorizationHeader(headers);
-    body = JSON.stringify(body);
-    const options = new RequestOptions({ // tslint:disable-line
-      headers: headers,
-      params: params,
-    });
-    return this.http.post('https://demo.openmf.org/fineract-provider/api/v1/client/' + clientId +
-        '/addresses?' + '&tenantIdentifier=default&pretty=true', body,
-        options)
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response.json();
-          }
-        )
-      );
+  /**
+   * @param {number} clientId
+   * @param {any} type
+   * @param {any} body
+   * @returns {Observable<any>}
+   */
+  postClientAddress(clientId: number, body: any, type: any): Observable<any> {
+    const httpParams = new HttpParams()
+      .set('type', type);
+    return this.http.post(`/clients/${clientId}` + '/addresses', body, { params: httpParams });
   }
 
-  putClientAddress(clientId: number, body: any, type: any) {
-    const headers = new Headers(); // tslint:disable-line
-    const params = new URLSearchParams(); // tslint:disable-line
-    params.set('type', type);
-    this.createAuthorizationHeader(headers);
-   // body = JSON.stringify(body);
-    const options = new RequestOptions({ // tslint:disable-line
-      headers: headers,
-      params: params,
-    });
-    return this.http.put('https://demo.openmf.org/fineract-provider/api/v1/client/' + clientId +
-        '/addresses?' + 'tenantIdentifier=default', body,
-        options)
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response.json();
-          }
-        )
-      );
+  /**
+   * @param {number} clientId
+   * @param {any} type
+   * @param {any} body
+   * @returns {Observable<any>}
+   */
+  putClientAddress(clientId: number, body: any, type: any): Observable<any> {
+    const httpParams = new HttpParams()
+      .set('type', type);
+    return this.http.put(`/clients/${clientId}` + '/addresses', body, { params: httpParams });
   }
 
-  getClientIdentifierTemplate(clientId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/identifiers/template?tenantIdentifier=default', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response['allowedDocumentTypes'];
-
-          }
-        )
-      );
+  /**
+   * @param {number} clientId
+   * @returns {Observable<any>} Client template
+   */
+  getClientIdentifierTemplate(clientId: number): Observable<any> {
+    return this.http.get(`/clients/${clientId}` + '/identifiers/template');
   }
 
-  getClientIdentifiers(clientId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/identifiers/?tenantIdentifier=default', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response = response.json();
-            return response;
-
-          }
-        )
-      );
+  /**
+   * @param {number} clientId
+   * @returns {Observable<any>} Client Identifiers
+   */
+  getClientIdentifiers(clientId: number): Observable<any> {
+    return this.http.get(`/clients/${clientId}` + '/identifiers');
   }
 
-  postClientIdentifier(clientId: number, body: any) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.post('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/identifiers/?tenantIdentifier=default', body, {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response.json();
-          }
-        )
-      );
+  /**
+   * @param {number} clientId
+   * @param {any} body
+   * @returns {Observable<any>}
+   */
+  postClientIdentifier(clientId: number, body: any): Observable<any> {
+    return this.http.post(`/clients/${clientId}` + '/identifiers', body);
   }
 
+  /**
+   * @param {number} clientId
+   * @param {number} identifierId
+   * @returns {Observable<any>}
+   */
   deleteClientIdentifier(clientId: number, identifierId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.delete('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/identifiers/' + identifierId + '/?tenantIdentifier=default', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response.json();
-          }
-        )
-      );
-  }
-  postClientDocument(clientId: number, body: any) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    headers.append('Content-Type', null);
-    headers.append('Accept', 'multipart/form-data');
-    body = JSON.stringify(body);
-
-    return this.http.post('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/documents/?tenantIdentifier=default', body, {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response.json();
-          }
-        )
-      );
+    return this.http.delete(`/clients/${clientId}` + `/identifiers/${identifierId}`);
   }
 
-  getClientDocuments(clientId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/documents/?tenantIdentifier=default', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            return response.json();
-          }
-        )
-      );
+  /**
+   * @param {number} clientId
+   * @returns {Observable<any>}
+   */
+  postClientDocument(clientId: number, body: any): Observable<any> {
+    return this.http.post(`/clients/${clientId}` + '/documents', body);
   }
 
-  deleteClientDocuments(clientId: number, documentId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.delete('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/documents/' + documentId + '/?tenantIdentifier=default', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            response.json();
-          }
-        )
-      );
+  /**
+   * @param clientId
+   * @returns {Observable<any>} Clients Documents
+   */
+  getClientDocuments(clientId: number): Observable<any> {
+    return this.http.get(`/clients/${clientId}` + 'documents');
   }
 
+  /**
+   * @param {number} clientId
+   * @param {number} documentId
+   * @returns {Observable<any>}
+   */
+  deleteClientDocuments(clientId: number, documentId: number): Observable<any> {
+    return this.http.delete(`/clients/${clientId}` + `/documents/${documentId}`);
+  }
+
+  /**
+   * @param {number} clientId
+   * @param {number} documentId
+   * @returns {Observable<any>} Client documents
+   */
   downloadClientDocuments(clientId: number, documentId: number) {
-    const headers = new Headers(); // tslint:disable-line
-    this.createAuthorizationHeader(headers);
-    return this.http.get('https://demo.openmf.org/fineract-provider/api/v1/clients/' + clientId +
-        '/documents/' + documentId + '/attachment/?tenantIdentifier=default', {
-          headers: headers
-        })
-      .pipe(
-        map(
-          (response: Response) => { // tslint:disable-line
-            return response.json();
-          }
-        )
-      );
+    return this.http.get(`/clients/${clientId}` + `/documents/${documentId}`);
   }
-
-
-
-
 }
 
